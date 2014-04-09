ac610bf278b8731d80f633233a24cae444c5b6fc
Ankit Goyal <ankit3goyal@gmail.com>
2014-04-08 23:26:30 -0500

    adding pass runner skeleton

--- pass_runner.cpp
+++ pass_runner.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// vector with permutations of passorder
+vector<string> passOrder;
+
+void runOptimizationPasses(){
+
+    for( int i = 0; i < passOrder.size() ; ++i ) {
+        system(NULL);
+    }
+}
+
+// print operator for vector<string>
+void operator<<(ostream& os, vector<string> V){
+    for( vector<string>::iterator it = V.begin(); it != V.end() ; ++it ) {
+        os << *it << endl;
+    }
+}
 
 int main(){
     // open a file in read mode.
     ifstream infile; 
+    infile.open("events.1");
+
+    string opt_order;
+    while(getline(infile,opt_order)) {
+        passOrder.push_back(opt_order);
+    }
+
+    // this will potentially print the contents of events.1 file
+#ifdef VERBOSE
+    cout << passOrder;
+#endif
+
+    runOptimizationPasses();
 
 }
