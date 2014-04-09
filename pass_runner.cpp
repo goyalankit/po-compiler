582d4870ee62635665660cac32c8ec8d75a9e9f2
Ankit Goyal <ankit3goyal@gmail.com>
2014-04-09 00:21:50 -0500

    adding passrunner program

--- pass_runner.cpp
+++ pass_runner.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <stdlib.h> 
+#include <map>
+#include "omp.h"
+#include <time.h>
+
+#define NUM_THREADS 8
 
 using namespace std;
 
@@ -10,8 +16,25 @@ vector<string> passOrder;
 
 void runOptimizationPasses(){
 
+    map<string, float> optExecMap;
+    clock_t t1,t2;
+    float diff;
+
+#pragma omp parallel for private(t1, t2, diff)
     for( int i = 0; i < passOrder.size() ; ++i ) {
-        system(NULL);
+
+        string command = string("opt ") + passOrder[i] + string(" ./matrix.o >> /dev/null");
+
+#ifdef DEBUG
+        cout << "Running Command: " << command  << endl;
+#endif
+
+        t1 = clock();
+        system(command.c_str());
+        t2 = clock();
+
+        diff = (float)t2 - (float)t1;
+        optExecMap.insert( pair<string,float>(passOrder[i], diff) );
     }
 }
 
@@ -22,7 +45,13 @@ void operator<<(ostream& os, vector<string> V){
     }
 }
 
-int main(){
+int main(int argc, char** argv){
+    
+    int numThreads = NUM_THREADS;
+    if(argc > 1){
+        numThreads = atoi(argv[1]);
+    }
+
     // open a file in read mode.
     ifstream infile; 
     infile.open("events.1");
@@ -36,7 +65,11 @@ int main(){
 #ifdef VERBOSE
     cout << passOrder;
 #endif
-
+    
+    // set openmp number of threads
+    omp_set_num_threads(NUM_THREADS);
+    
     runOptimizationPasses();
 
+    return EXIT_SUCCESS;
 }
