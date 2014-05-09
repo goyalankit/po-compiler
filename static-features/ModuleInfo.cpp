191a6b7abda715138017658a11e3fa6d4047d2a4
Ankit Goyal <ankit3goyal@gmail.com>
2014-05-09 16:13:24 -0500

    adding module info pass

--- static-features/ModuleInfo.cpp
+++ static-features/ModuleInfo.cpp
@@ -33,106 +33,151 @@
 #include <fstream>
 #include <iostream>
 
- using namespace llvm;
- using namespace std;
+using namespace llvm;
+using namespace std;
 
- STATISTIC(LoopInstNum, "Counts number of instructions in a loop");
- STATISTIC(LoopNum, "Counts number of loops");
- STATISTIC(LoopBasicBlocksNum, "Counts number of basic blocks in a loop");
- STATISTIC(LoopDepth, "sum of all depths of loop. divide by loop num to get average depth");
- STATISTIC(LoopBranchInstNum, "Counts number of branch instructions in a loop");
- STATISTIC(LoopLoadInst, "Counts number of load instructions in a loop");
- STATISTIC(LoopStoreInst, "Counts number of store instructions in a loop");
+STATISTIC(LoopInstNum, "Counts number of instructions in a loop");
+STATISTIC(LoopNum, "Counts number of loops");
+STATISTIC(LoopBasicBlocksNum, "Counts number of basic blocks in a loop");
+STATISTIC(LoopDepth, "sum of all depths of loop. divide by loop num to get average depth");
+STATISTIC(LoopBranchInstNum, "Counts number of branch instructions in a loop");
+STATISTIC(LoopLoadInst, "Counts number of load instructions in a loop");
+STATISTIC(LoopStoreInst, "Counts number of store instructions in a loop");
+STATISTIC(LoopNumOperands, "Counts number of operands in a loop");
+STATISTIC(LoopNumMemInst, "Counts number of memory instructions in a loop");
+STATISTIC(NumOperations, "Counts number of operations in a loop");
 
+namespace 
+{
+    struct LICM : public LoopPass {
+        static char ID; // Pass identification, replacement for typeid
+        LICM() : LoopPass(ID) {}
 
-
- namespace 
- {
-   struct LICM : public LoopPass {
-     static char ID; // Pass identification, replacement for typeid
-     LICM() : LoopPass(ID) {}
-
-     struct Features {
-        int numberOfBasicBlocks;
-        int numberOfInstructions;
-        int loopDepth;
-     };
-
-
-     private:
-     bool mayThrow;
-     bool changed;
-     LoopInfo* LI;
-     DominatorTree* DT; 
-     Loop* CurrentLoop;
-     BasicBlock *Preheader;
-     Instruction *InsertPt;
-     std::vector<Features*> loopsFeature;
-
-     virtual bool runOnLoop(Loop *L, LPPassManager &LPM) 
-     {
-          LI = &getAnalysis<LoopInfo>();
-          DT = &getAnalysis<DominatorTree>();
-          CurrentLoop = L;
-          ++LoopNum;
-          Features* F = new Features;
-
-          Preheader = L->getLoopPreheader();
-
-          mayThrow = false;
-          int numOfInstr = 0; // number of instructions
-          int numOfBB = 0; // number of basic blocks
-          int numOfBranchInstruction = 0;
-
-          for (Loop::block_iterator b = L->block_begin(), be = L->block_end();b !=be; ++b)
-         {
-
-             ++LoopBasicBlocksNum;
-             
-             for (BasicBlock::iterator i = (*b)->begin(), ie = (*b)->end(); i != ie; ++i)
-             {
-                 if (isa<BranchInst>(*i))
-                     ++LoopBranchInstNum;
-                 else if(isa<LoadInst>(*i))
-                     ++LoopLoadInst;
-                 else if(isa<StoreInst>(*i))
-                     ++LoopStoreInst;
-
-                 ++LoopInstNum;
-             }
-
-             LoopDepth += L->getLoopDepth();
-         }
-            
-         F->loopDepth = L->getLoopDepth();
-
-         errs() << "Num of instructions " << numOfInstr << " num of blocks " << numOfBB << " loop depth" << F->loopDepth <<" \n";
-
-       return false;
-     }
-
-     // We don't modify the program, so we preserve all analyses
-     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
-       AU.setPreservesAll();
-       AU.addRequired<LoopInfo>();
-       AU.addRequired<DominatorTree>();
-     }
-
-     bool inSubLoop(BasicBlock *bb)
-     {
-     
-      assert(CurrentLoop->contains(bb) && "Valid if BB is IN the loop");
-      return LI->getLoopFor(bb) != CurrentLoop;
-    
-     }
+        struct Features {
+            int numberOfBasicBlocks;
+            int numberOfInstructions;
+            int loopDepth;
+        };
 
 
+        private:
+        bool mayThrow;
+        bool changed;
+        LoopInfo* LI;
+        DominatorTree* DT; 
+        Loop* CurrentLoop;
+        BasicBlock *Preheader;
+        Instruction *InsertPt;
+        std::vector<Features*> loopsFeature;
 
-   };
- 
+        bool isOperation(unsigned int op) {
+            switch(op){
 
- char LICM::ID = 0;
- RegisterPass<LICM> X("module-info", "LICM");
+                case Instruction::Add:
+                case Instruction::FAdd:
+                case Instruction::Sub:
+                case Instruction::FSub:
+                case Instruction::Mul:
+                case Instruction::FMul:
+                case Instruction::UDiv:
+                case Instruction::SDiv:
+                case Instruction::FDiv:
+                case Instruction::URem:
+                case Instruction::SRem:
+                case Instruction::FRem:
+                case Instruction::Shl:
+                case Instruction::LShr:
+                case Instruction::AShr:
+                case Instruction::And:
+                case Instruction::Or:
+                case Instruction::Xor:
+
+
+                    {
+                        ++NumOperations;               
+                        break;
+                    }
+
+                default:
+                    break;
+
+            }
+        }
+
+        virtual bool runOnLoop(Loop *L, LPPassManager &LPM) 
+        {
+            LI = &getAnalysis<LoopInfo>();
+            DT = &getAnalysis<DominatorTree>();
+            CurrentLoop = L;
+            ++LoopNum;
+            Features* F = new Features;
+
+            Preheader = L->getLoopPreheader();
+
+            mayThrow = false;
+            int numOfInstr = 0; // number of instructions
+            int numOfBB = 0; // number of basic blocks
+            int numOfBranchInstruction = 0;
+
+            for (Loop::block_iterator b = L->block_begin(), be = L->block_end();b !=be; ++b)
+            {
+
+                ++LoopBasicBlocksNum;
+
+                for (BasicBlock::iterator i = (*b)->begin(), ie = (*b)->end(); i != ie; ++i)
+                {
+
+                    Instruction * I = &*i;
+                    ++LoopInstNum;
+
+                    if (isa<BranchInst>(*i))
+                        ++LoopBranchInstNum;
+                    else if(isa<LoadInst>(*i))
+                        ++LoopLoadInst;
+                    else if(isa<StoreInst>(*i))
+                        ++LoopStoreInst;
+
+                    if(isa<AllocaInst>(*i) || isa<LoadInst>(*i) || isa<StoreInst>(*i))
+                        ++LoopNumMemInst;
+
+
+                    if(isOperation(I->getOpcode()))
+                        ++NumOperations;
+
+                    LoopNumOperands += I->getNumOperands();
+                }
+
+            }
+
+            LoopDepth += L->getLoopDepth();
+
+            errs() << "Num of instructions " << numOfInstr << " num of blocks " << numOfBB << " loop depth" << F->loopDepth <<" \n";
+
+            return false;
+        }
+
+        // We don't modify the program, so we preserve all analyses
+        virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+            AU.setPreservesAll();
+            AU.addRequired<LoopInfo>();
+            AU.addRequired<DominatorTree>();
+        }
+
+        bool inSubLoop(BasicBlock *bb)
+        {
+
+            assert(CurrentLoop->contains(bb) && "Valid if BB is IN the loop");
+            return LI->getLoopFor(bb) != CurrentLoop;
+
+        }
+
+
+
+    };
+
+
+    char LICM::ID = 0;
+    RegisterPass<LICM> X("module-info", "LICM");
 
 }
 
