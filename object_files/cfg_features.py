4351f0c62636dd30ac1613da7182d0004b86b865
Ankit Goyal <ankit3goyal@gmail.com>
2014-05-09 18:37:03 -0500

    adding all the files

---
+++ object_files/cfg_features.py
@@ -0,0 +1,24 @@
+import sys
+import os.path
+from collections import defaultdict
+
+filenames = os.listdir('prog1')
+
+num_nodes = []
+num_edges = []
+diameter = []
+path_length = []
+cluster_coeff = []
+avg_degree = []
+density = []
+
+
+for i in filenames:
+    
+    
+
+
+features[filename]["features"] = 
+
+
+
